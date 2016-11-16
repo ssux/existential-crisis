6a1a99d4b0d682115ca93f766896a08017c48a3b
ssux <ssux@pratt.edu>
2016-11-16 22:14:38 +0000

    commit existential crisis

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$("#o1").click(function () {
+    $(".options").css("background", "red");
+    $("#o2").hide();
+    $(".background").css("background-image", "url(../images/backgroundKierke.jpg"); 
+})
