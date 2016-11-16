2824bae02c8020b4b7b7c1ae01887d0f77d6b2e7
ssux <ssux@pratt.edu>
2016-11-16 22:32:14 +0000

    ugh

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+
+$("#option3").click (function () {
+    $(".text").hide();
+})
+
 $("#o1").click(function () {
-    $(".options").css("background", "red");
+    $(".options").css("background", "#310102");
     $("#o2").hide();
     $(".background").css("background-image", "url(../images/backgroundKierke.jpg"); 
 })
